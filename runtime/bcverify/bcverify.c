8c1d07693d96a023d81d4111c235b19aa69a731a
Manqing Li <manqingl@ca.ibm.com>
2024-10-30 10:29:10 -0400

    Do RESET_VERIFY_ERROR at the start of j9bcv_verifyBytecodes() part 2
    Signed-off-by: Manqing Li <manqingl@ca.ibm.com>

--- runtime/bcverify/bcverify.c
+++ runtime/bcverify/bcverify.c
@@ -2433,6 +2433,9 @@ j9bcv_verifyBytecodes (J9PortLibrary * portLib, J9Class * clazz, J9ROMClass * ro
 
 	verifyData->romClass = romClass;
 	RESET_VERIFY_ERROR(verifyData);
+	verifyData->errorPC = (UDATA) 0;
+	verifyData->errorModule = 0;
+	verifyData->errorCode = 0;
 
 	verifyData->romClassInSharedClasses = j9shr_Query_IsAddressInCache(verifyData->javaVM, romClass, romClass->romSize);
 
